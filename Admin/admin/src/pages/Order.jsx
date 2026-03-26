3d85453b052af36067aa6c0f5312241f0d33b196
UsmmmanT <k224478@nu.edu.pk>
2026-03-26 21:37:07 +0500

    Fixed order status filter.

--- Admin/admin/src/pages/Order.jsx
+++ Admin/admin/src/pages/Order.jsx
@@ -81,7 +81,7 @@ const Order = ({ token }) => {
         <select
           value={statusFilter}
           onChange={handleStatusFilter}
-          className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition outline-none'
+          className='px-4 py-2 bg-white-500 text-black rounded hover:bg-blue-600 transition outline-none'
         >
           {statusOptions.map(option => (
             <option key={option} value={option}>{option}</option>
